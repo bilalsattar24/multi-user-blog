0e444725adca80fdfef24e66b7e7b44da7471bc4
bilalsattar24 <bilalsattar24@gmail.com>
2016-12-27 02:49:47 -0800

    handler for front page

--- blog.py
+++ blog.py
@@ -10,16 +10,26 @@ template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
                                autoescape = True)
 
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 #base class for handler. other handlers inherit from 
-class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        self.response.write('Hello blog!')
+class BaseHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    def render_str(self, template, **params):
+        return render_str(template, **params)
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
 
-class TestHandler(webapp2.RequestHandler):
+class TestHandler(BaseHandler):
     def get(self):
         self.response.write('Test passed')
 
+class FrontPage(BaseHandler):
+    def get(self):
+        self.render("front-page.html")
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
+    ('/', FrontPage),
     ('/test', TestHandler)
 ], debug=True)
